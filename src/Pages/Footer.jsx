cd71f60994384ba2b29941fb31da095d1509094e
Nitin Patel <156987426+nitin-pat@users.noreply.github.com>
2025-07-27 22:37:51 +0530

    mmmm

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -3,8 +3,8 @@ import "./Footer.css";
 import { Link } from "react-router-dom";
 import linked from "../assets/linkedin.png";
 import insta from "../assets/instagram.png";
-import twitter from "../assets/twitter.png"
-import facebook from "../assets/facebook-media.png"
+import twitter from "../assets/twitter.png";
+import facebook from "../assets/facebook-media.png";
 
 const Footer = () => {
   return (
@@ -12,36 +12,53 @@ const Footer = () => {
       <div className="body-section">
         <div className="address-section">
           <h2>Address</h2> <hr />
-          <p>Kachatipur Kannauj Uttar Pradesh,209723 <br />
-          Landmark: Tirwa road Indrgarh Kannauj Uttar Pradesh <br />
-          Phone: +91-9026532143
+          <p>
+            Kachatipur Kannauj Uttar Pradesh,209723 <br />
+            Landmark: Tirwa road Indrgarh Kannauj Uttar Pradesh <br />
+            Phone: +91-9026532143
           </p>
         </div>
         <div className="media">
           <div className="media-section">
-            <Link to={"https://www.linkedin.com/in/n-technologies123/"}>
-              <img src={linked} alt="img not found" />
-            </Link>
+            <a
+              href="https://www.linkedin.com/in/n-technologies123"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={linked} alt="LinkedIn" />
+            </a>
           </div>
           <div className="media-section">
-            <Link to={"https://www.linkedin.com/"}>
-              <img src={insta} alt="img not found" />
-            </Link>
+            <a
+              href="https://www.instagram.com/codeconductor"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={insta} alt="Instagram" />
+            </a>
           </div>
           <div className="media-section">
-            <Link to={"https://www.twitter.com/"}>
-              <img src={twitter} alt="img not found" />
-            </Link>
+            <a
+              href="https://x.com/nitin_patel61580"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={twitter} alt="Twitter" />
+            </a>
           </div>
           <div className="media-section">
-            <Link to={"https://www.facebook.com/"}>
-              <img src={facebook} alt="img not found" />
-            </Link>
+            <a
+              href="https://www.instagram.com/codeconductor"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={facebook} alt="Facebook" />
+            </a>
           </div>
         </div>
       </div>
       <div className="copy-right-section">
-        <p>Copyright &copy; 2025 N-Technologies || All Rights are Reserved</p>
+        <p>Copyright &copy; 2025 N-Technologies || All Rights Reserved</p>
       </div>
     </div>
   );
